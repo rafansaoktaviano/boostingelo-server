c3ff296301f7c4d2746732c4bd77a3fa8f7b6638
Rafansa Oktaviano <ravansaoktaviano@gmail.com>
2025-07-01 22:29:47 +0700

    fix error supabase on stripe webhook

--- dist/src/config/supabase.js
+++ dist/src/config/supabase.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const supabase_js_1 = require("@supabase/supabase-js");
 const supabaseUrl = process.env.SUPABASE_URL || '';
-const supabaseAnonKey = process.env.SUPABASE_SERVICE_ROLE || '';
-const supabase = (0, supabase_js_1.createClient)(supabaseUrl, supabaseAnonKey, {
-    global: {
-        headers: { Authorization: `Bearer ${process.env.JWT_SERVICE_ROLE}` },
-    },
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE || '';
+const supabase = (0, supabase_js_1.createClient)(supabaseUrl, supabaseServiceKey, {
+    auth: {
+        persistSession: false
+    }
 });
 exports.default = supabase;
--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,12 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.SUPABASE_URL || ''
-const supabaseAnonKey = process.env.SUPABASE_SERVICE_ROLE || ''
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE || ''
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  global: {
-    headers: { Authorization: `Bearer ${process.env.JWT_SERVICE_ROLE}` },
-  },
+const supabase = createClient(supabaseUrl, supabaseServiceKey, {
+  auth:{
+    persistSession: false
+  }
 })
 
 export default supabase
